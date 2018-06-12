3b8e15cbba469142b2132c1e3893c2fdfe3b0e57
Nate Christ <nat1597@hotmail.com>
2018-06-11 19:43:01 -0500

    app/slides update

---
+++ vue-demo/Component/Vue-File.vue
@@ -0,0 +1,28 @@
+<!--sample-component.vue-->
+<template>
+  <!-- injected into parent component -->
+  <div>
+    <p>HTML Structure</p>
+    <div class="someClass">{{ text }}</div>
+  </div>
+  <!-- injected into parent component -->
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        text: 'Welcome to ITAG!'
+      }
+    }
+  }
+</script>
+
+<style>
+  .someClass{
+    color: red;
+    background-color: black;
+    height: 500px;
+    width: 500px;
+  }
+</style>
